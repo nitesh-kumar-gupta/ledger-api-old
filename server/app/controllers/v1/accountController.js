62ec365bd82cb953c2286a0112e95ab1cf2a6dd8
Nitesh Kumar Gupta <nitesh.gupta@venturepact.com>
2017-08-01 02:31:35 +0530

    account add api changes

--- server/app/controllers/v1/accountController.js
+++ server/app/controllers/v1/accountController.js
@@ -15,7 +15,6 @@ const accountController = {
         Response.response(res, data);
     },
     addAccount: async (req, res) => {
-        req.body.customer = req.params.id;
         const _accountService = new accountService(req.body);
         const account = await _accountService.addAccount();
         Response.response(res, account);
